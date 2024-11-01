218f1d7990816bb1fc0dfd62ff89a9684b5d9963
amhama <Descenderdz@gmail.com>
2024-11-01 14:53:46 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,23 @@
+
+
+d= 2 ,   h=14
+
+
+d=0   , h=14
+
+d=2   ,h=23
+
+
+if( ( d>=1 and d<7  )          and   (   h<=17 and h=>8 )) then 
+
+           write("open");
+ endif;                  
+
+if(d<1 and d>7 and  h>17 and h<8)
+              write("closed") ;
+
+ endif;                  
+    
+
+
+        
